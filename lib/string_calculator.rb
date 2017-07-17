1f92d2efb0eacb21726f5cd157eb6e156f3ee040
Meep <mwraich@financeit.io>
2017-07-17 19:30:26 -0400

    requirement 6 passing

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,8 @@
 module StringCalculator
 
   def self.add(string)
-    numbers = string.split(',').map(&:to_i)
+    formated_string = string.tr("\n",',')
+    numbers = formated_string.split(',').map(&:to_i)
     numbers.inject(0){|sum,n| sum + n }
   end
 
