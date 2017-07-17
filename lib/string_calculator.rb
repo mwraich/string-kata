3bc520ef9bb45dca4e9ef2ddbdd94c7eca95d949
Meep <mwraich@financeit.io>
2017-07-17 19:41:33 -0400

    test passing, ugly, but effective

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,7 @@
 module StringCalculator
 
   def self.add(string)
-    formated_string = string.tr("\n",',')
+    formated_string = string.tr("//;\n", ',')
     numbers = formated_string.split(',').map(&:to_i)
     numbers.inject(0){|sum,n| sum + n }
   end
